7d7d1e7ee2eaecf4d377462c81c629e46a84b001
Bengü Baştuğ <bengubastug@gmail.com>
2023-04-05 19:12:36 +0300

    add streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,7 @@
+import streamlit as st
+import seaborn as sns
 import pandas as pd
+st.title("Evolution of World Happiness")
+st.write("In this project we want to analisys the **World Happiness** in the last years and try to understand what contribute or not to the results")
+st.sidebar.header("What should we know about the World Hapiness")
+st.sidebar.slider('Select the Year', 2015, 2021)
