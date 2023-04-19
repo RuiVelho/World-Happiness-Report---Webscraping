21584d2689487911db12dc091c098b889eebf307
Bengü Baştuğ <bengubastug@gmail.com>
2023-04-19 16:01:14 +0300

    left bar add

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,8 @@ st.sidebar.header("World Happiness Analysis")
 df2019 = pd.read_csv("https://raw.githubusercontent.com/MauricioConceicao123/World-Happiness-Report---Webscraping/main/2019.csv")
 df2018 = pd.read_csv('https://raw.githubusercontent.com/MauricioConceicao123/World-Happiness-Report---Webscraping/main/2018.csv')
 #################
-choice = st.sidebar.radio("Select the Year", ('2018','2019'))
-if choice == '2018':
+choice = st.sidebar.radio("Select the Topic", ('Overall Happiness','Happiness by Region','Wealth Potential','Social Support','Correlations'))
+if choice == 'Overall Happiness':
     st.header("World Happiness Analysis 2018")
     st.subheader("Here some explanation")
     def page_plot1():
@@ -47,8 +47,92 @@ if choice == '2018':
         pages.keys()
     )
     pages[selected_page]()
-if choice == '2019':
+if choice == 'Happiness by Region':
     st.header("World Happiness Analysis 2019")
+    st.subheader("Here some explanation")
+    def page_plot1():
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("here column 1")
+            #some graphs or tables
+        with col2:
+            st.write("here column 2")
+            #some graphs or tables
+    def page_plot2():
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("here column 1")
+            #some graphs or tables
+        with col2:
+            st.write("here column 2")
+            #some graphs or tables
+    pages = {
+        "Plot1": page_plot1,    #names will change, don't worry :)
+        "Plot2": page_plot2     #names will change, don't worry :)
+    }
+    selected_page = st.selectbox(
+        "Choose Page",
+        pages.keys()
+    )
+    pages[selected_page]()
+if choice == 'Wealth Potential':
+    st.header("World Happiness Analysis 2018")
+    st.subheader("Here some explanation")
+    def page_plot1():
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("here column 1")
+            #some graphs or tables
+        with col2:
+            st.write("here column 2")
+            #some graphs or tables
+    def page_plot2():
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("here column 1")
+            #some graphs or tables
+        with col2:
+            st.write("here column 2")
+            #some graphs or tables
+    pages = {
+        "Plot1": page_plot1,    #names will change, don't worry :)
+        "Plot2": page_plot2     #names will change, don't worry :)
+    }
+    selected_page = st.selectbox(
+        "Choose Page",
+        pages.keys()
+    )
+    pages[selected_page]()
+if choice == 'Social Support':
+    st.header("World Happiness Analysis 2018")
+    st.subheader("Here some explanation")
+    def page_plot1():
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("here column 1")
+            #some graphs or tables
+        with col2:
+            st.write("here column 2")
+            #some graphs or tables
+    def page_plot2():
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("here column 1")
+            #some graphs or tables
+        with col2:
+            st.write("here column 2")
+            #some graphs or tables
+    pages = {
+        "Plot1": page_plot1,    #names will change, don't worry :)
+        "Plot2": page_plot2     #names will change, don't worry :)
+    }
+    selected_page = st.selectbox(
+        "Choose Page",
+        pages.keys()
+    )
+    pages[selected_page]()
+if choice == 'Correlations':
+    st.header("World Happiness Analysis 2018")
     st.subheader("Here some explanation")
     def page_plot1():
         col1, col2 = st.columns(2)
