ddaf545926a07b72ea2c8646b19eefc8c8919b5e
Bengü Baştuğ <bengubastug@gmail.com>
2023-04-19 22:37:20 +0300

    add regional plots

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,7 @@ st.sidebar.header("World Happiness Analysis")
 #dataset will come here:
 df2019 = pd.read_csv("https://raw.githubusercontent.com/MauricioConceicao123/World-Happiness-Report---Webscraping/main/2019.csv")
 df2018 = pd.read_csv('https://raw.githubusercontent.com/MauricioConceicao123/World-Happiness-Report---Webscraping/main/2018.csv')
+df2015 = pd.read_csv("https://raw.githubusercontent.com/MauricioConceicao123/World-Happiness-Report---Webscraping/main/2015.csv")
 #################
 choice = st.sidebar.radio("Select the Topic", ('Overall Happiness','Happiness by Region','Wealth Potential','Social Support','Correlations'))
 if choice == 'Overall Happiness':
@@ -48,16 +49,56 @@ if choice == 'Overall Happiness':
     )
     pages[selected_page]()
 if choice == 'Happiness by Region':
-    st.header("World Happiness Analysis 2019")
+    st.header("Happiness by Region 2019")
     st.subheader("Here some explanation")
+    
     def page_plot1():
         col1, col2 = st.columns(2)
         with col1:
             st.write("here column 1")
+            df2019_mean_region = pd.read_csv('https://raw.githubusercontent.com/MauricioConceicao123/World-Happiness-Report---Webscraping/main/df2019_mean_region.csv')
             #some graphs or tables
+            fig, ax = plt.subplots(1,1)
+            viz_1 = sns.barplot(y = df2019_mean_region.Region,
+                                x = df2019_mean_region.happiness_score,
+                                orient='h',
+                                palette="ch:s=.25,rot=-.25",
+                                ax=ax)
+            plt.title("Average Happiness Score per Region")
+            plt.xlabel("Average Happiness Score") 
+            plt.ylabel("")
+            plt.xlim(left=4)
+            plt.grid(axis='x', alpha=.3)
+            st.pyplot(viz_1.figure)
+            # second graph: move it somewhere else #
+            df2019_range = pd.read_csv('https://raw.githubusercontent.com/MauricioConceicao123/World-Happiness-Report---Webscraping/main/df2019_range.csv')
+            fig, ax = plt.subplots(1,1)
+            viz_3 = sns.barplot(y = df2019_range.Region,
+                                x = df2019_range.happiness_score,
+                                orient='h',
+                                palette="ch:s=.25,rot=-.25",
+                                ax=ax)
+            plt.title("Range of Happiness Score per Region")
+            plt.xlabel("Range of Happiness Score") 
+            plt.ylabel("")
+            plt.grid(axis='x', alpha=.3)
+            st.pyplot(viz_3.figure)
         with col2:
             st.write("here column 2")
+            df2019_happinest_6 = pd.read_csv('https://raw.githubusercontent.com/MauricioConceicao123/World-Happiness-Report---Webscraping/main/df2019_happinest_6.csv')
             #some graphs or tables
+            fig, ax = plt.subplots(1,1)
+            viz_2 = sns.barplot(y = df2019_happinest_6.Region,
+                                x = df2019_happinest_6.happiness_score,
+                                orient='h',
+                                palette="ch:s=.25,rot=-.25",
+                                ax=ax)
+            plt.title("Happiness score above 6.0")
+            plt.xlabel("Number of countries") 
+            plt.ylabel("")
+            plt.xticks([0, 3, 6, 9, 12, 15, 18])
+            plt.grid(axis='x', alpha=.3)
+            st.pyplot(viz_2.figure)
     def page_plot2():
         col1, col2 = st.columns(2)
         with col1:
